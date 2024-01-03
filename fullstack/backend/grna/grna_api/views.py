70ad93a4706aacb43e6a7773d90a8b6eef895ec0
Stan <stanbarkmeijer@hotmail.com>
2024-01-03 16:29:19 +0100

    Frontend

--- fullstack/backend/grna/grna_api/views.py
+++ fullstack/backend/grna/grna_api/views.py
@@ -20,15 +20,6 @@ class PredictionListApiView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    # def get_one(self, request, *args, **kwargs):
-    #     """
-    #     Returns a single prediction.
-    #     """
-    #     prediction = Prediction.objects.get(pk=kwargs["pk"])
-    #     serializer = PredictionSerializer(prediction)
-
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
-    
     def post(self, request, *args, **kwargs):
         """
         Creates a new prediction.
@@ -50,44 +41,6 @@ class PredictionListApiView(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
-        
-    # def put(self, request, *args, **kwargs):
-    #     """
-    #     Updates a prediction.
-    #     """
-    #     prediction = Prediction.objects.get(pk=kwargs["pk"])
-
-    #     if request.user.id != prediction.user.id or request.user.is_superuser == False:
-    #         return Response(
-    #             {"error": "You do not have permission to edit this prediction."},
-    #             status=status.HTTP_403_FORBIDDEN
-    #         )
-
-    #     serializer = PredictionSerializer(prediction, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-        
-    #     return Response(
-    #         serializer.errors,
-    #         status=status.HTTP_400_BAD_REQUEST
-    #     )
-    
-    # def delete(self, request, *args, **kwargs):
-    #     """
-    #     Deletes a prediction.
-    #     """
-    #     prediction = Prediction.objects.get(pk=kwargs["pk"])
-
-    #     if request.user.id != prediction.user.id or request.user.is_superuser == False:
-    #         return Response(
-    #             {"error": "You do not have permission to delete this prediction."},
-    #             status=status.HTTP_403_FORBIDDEN
-    #         )
-
-    #     prediction.delete()
-
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 class PredictionDetailApiView(APIView):
     permission_classes = [permissions.AllowAny]
