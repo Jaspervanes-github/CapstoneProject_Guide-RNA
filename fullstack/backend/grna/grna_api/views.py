b504be227b9f6254d0958dfe979da9259e716a1c
Stan Barkmeijer <stan@barkmeijer.com>
2024-01-10 10:52:40 +0100

    Commit

--- fullstack/backend/grna/grna_api/views.py
+++ fullstack/backend/grna/grna_api/views.py
@@ -20,15 +20,6 @@ class PredictionListApiView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
     
-    # def get_one(self, request, *args, **kwargs):
-    #     """
-    #     Returns a single prediction.
-    #     """
-    #     prediction = Prediction.objects.get(pk=kwargs["pk"])
-    #     serializer = PredictionSerializer(prediction)
-
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
-    
     def post(self, request, *args, **kwargs):
         """
         Creates a new prediction.
@@ -37,7 +28,7 @@ class PredictionListApiView(APIView):
             "dna": request.data.get("dna"),
             "grna": request.data.get("grna"),
             "score": request.data.get("score"),
-            "user": request.user.id
+            "user": request.user.id if request.user.is_authenticated else None
         }
 
         serializer = PredictionSerializer(data=data)
@@ -50,44 +41,6 @@ class PredictionListApiView(APIView):
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
         )
-        
-    # def put(self, request, *args, **kwargs):
-    #     """
-    #     Updates a prediction.
-    #     """
-    #     prediction = Prediction.objects.get(pk=kwargs["pk"])
-
-    #     if request.user.id != prediction.user.id or request.user.is_superuser == False:
-    #         return Response(
-    #             {"error": "You do not have permission to edit this prediction."},
-    #             status=status.HTTP_403_FORBIDDEN
-    #         )
-
-    #     serializer = PredictionSerializer(prediction, data=request.data)
-    #     if serializer.is_valid():
-    #         serializer.save()
-    #         return Response(serializer.data, status=status.HTTP_200_OK)
-        
-    #     return Response(
-    #         serializer.errors,
-    #         status=status.HTTP_400_BAD_REQUEST
-    #     )
-    
-    # def delete(self, request, *args, **kwargs):
-    #     """
-    #     Deletes a prediction.
-    #     """
-    #     prediction = Prediction.objects.get(pk=kwargs["pk"])
-
-    #     if request.user.id != prediction.user.id or request.user.is_superuser == False:
-    #         return Response(
-    #             {"error": "You do not have permission to delete this prediction."},
-    #             status=status.HTTP_403_FORBIDDEN
-    #         )
-
-    #     prediction.delete()
-
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 
 class PredictionDetailApiView(APIView):
     permission_classes = [permissions.AllowAny]
