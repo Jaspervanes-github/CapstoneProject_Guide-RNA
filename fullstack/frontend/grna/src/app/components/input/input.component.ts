c9f961bce38372fa9ed6e44eb3a1a6b81458182f
Stan Barkmeijer <stanbarkmeijer@hotmail.com>
2024-01-18 11:43:39 +0100

    Addition

--- fullstack/frontend/grna/src/app/components/input/input.component.ts
+++ fullstack/frontend/grna/src/app/components/input/input.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-input',
@@ -9,9 +10,31 @@ import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 export class InputComponent {
   dnaForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private _httpClient: HttpClient
+  ) {
     this.dnaForm = this.fb.group({
       dnaSequence: ['', [Validators.required, Validators.pattern('^[ATCGatcg]*$')]]
+    });
+  }
+
+  get dnaSequence() {
+    return this.dnaForm.get('dnaSequence');
+  }
+
+  get isDnaSequenceValid() {
+    return this.dnaSequence?.valid;
+  }
+
+  submitPrediction() {
+    let port = 8000;
+    let dna = this.dnaSequence?.value;
+
+    console.log(`http://localhost:${port}}/api/predictions`, `dna: ${dna}`);
+
+    this._httpClient.post<any>(`http://localhost:${port}}/api/predictions`, { dna }).subscribe(data => {
+      console.log(data);
     });
   }
 }
